2b731415bf442ab6de94c5e54373f3b78dcdcd88
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2023-05-05 17:11:50 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Magic Auth GitHub Social Login
+
+Please [click here](https://magic.link/docs/auth/login-methods/social-logins/integration/social-providers/github) for full implementation instructions
---
+++ src/pages/Login.js
@@ -0,0 +1,27 @@
+import { magic } from "../lib/magic";
+import { FaGitlab } from "react-icons/fa";
+
+const Login = () => {
+  const handleSocialLogin = async () => {
+    try {
+      await magic.oauth.loginWithRedirect({
+        provider: "gitlab",
+        redirectURI: new URL("/dashboard", window.location.origin).href,
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <div className="container">
+      <h1>Magic Auth + GitLab</h1>
+      <button onClick={handleSocialLogin}>
+        <FaGitlab size={"2.5rem"} />
+        Log in with GitLab
+      </button>
+    </div>
+  );
+};
+
+export default Login;
